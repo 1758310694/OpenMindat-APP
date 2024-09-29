897f2fe7767435aad868320b6a44538ae7d0e791
1758310694 <135298824+1758310694@users.noreply.github.com>
2024-09-29 19:43:54 +0800

    Add files via upload

---
+++ components/button/button.vue
@@ -0,0 +1,56 @@
+<template>
+  <view class="buttons-container">
+    <view class="button" @click="createRecord">
+      <image src="/static/history_1.png" alt="创建记录"></image>
+      <text>创建记录</text>
+    </view>
+    <view class="button" @click="viewHistory">
+      <image src="/static/history_icon.png" alt="历史记录"></image>
+      <text>历史记录</text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  methods: {
+    createRecord() {
+      uni.showToast({
+        title: '创建记录',
+        icon: 'success'
+      });
+    },
+    viewHistory() {
+      uni.showToast({
+        title: '历史记录',
+        icon: 'success'
+      });
+    }
+  }
+}
+</script>
+
+<style>
+.buttons-container {
+  display: flex;
+  justify-content: space-around;
+  margin: 20px 0;
+}
+
+.button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+}
+
+.button image {
+  width: 50px;
+  height: 50px;
+}
+
+.button text {
+  margin-top: 10px;
+  font-size: 16px;
+}
+</style>
---
+++ components/navbar/navbar.vue
@@ -0,0 +1,66 @@
+<template>
+	<view>
+		<view class="example-body">
+			<uni-nav-bar shadow statusBar :fixed="false" color="#333333" background-color="#FFFFFF" right-icon="scan">
+				<view class="input-view">
+					<uni-icons class="input-uni-icon" type="search" size="22" color="#666666" />
+					<input confirm-type="search" class="nav-bar-input" type="text" placeholder="输入搜索关键词">
+				</view>
+			</uni-nav-bar>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniNavBar from '@/components/uni-nav-bar/uni-nav-bar.vue'
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		components: {
+			uniNavBar
+		}
+	}
+</script>
+
+<style lang="scss">
+	.input-view {
+		/* #ifndef APP-PLUS-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		width: 600rpx;
+		flex: 1;
+		background-color: #f8f8f8;
+		height: 30px;
+		border-radius: 15px;
+		padding: 0 15px;
+		flex-wrap: nowrap;
+		margin: 7px 0;
+		line-height: 30px;
+	}
+
+	.input-uni-icon {
+		line-height: 30px;
+	}
+
+	.nav-bar-input {
+		height: 30px;
+		line-height: 30px;
+		/* #ifdef APP-PLUS-NVUE */
+		width: 370rpx;
+		/* #endif */
+		padding: 0 5px;
+		font-size: 28rpx;
+		background-color: #f8f8f8;
+	}
+
+	.example-body {
+		padding: 0;
+	}
+</style>
